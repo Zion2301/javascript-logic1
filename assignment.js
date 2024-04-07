78a772e69e8d3cd64cee82b0397cf43998952a60
Zion <omogbemezion@gmail.com>
2024-04-07 10:52:51 -0700

    first commit

--- assignment.js
+++ assignment.js
@@ -334,3 +334,12 @@
 //      }
 //  }
 
+// 26 without doubles
+// function withoutDoubles(die1, die2, noDoubles){
+//     if (noDoubles && die1 === die2) {
+//         die1 = (die1 % 6) + 1;
+//     }
+
+//     return die1 + die2;
+// }
+
